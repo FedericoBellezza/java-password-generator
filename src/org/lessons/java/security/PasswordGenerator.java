bfcd57a8e3af0a101ca0d9b07e31f2731506a5d7
FedericoBellezza <federico.bellezza2001@gmail.com>
2025-02-26 15:33:57 +0100

    aggiunto il modo di far diventare la prima lettera maiuscola

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -9,13 +9,19 @@ public class PasswordGenerator {
 
         System.out.println("Ciao, sono un generatore di password, qual'è il tuo nome?");
         String nome = input.nextLine();
+        nome = Character.toUpperCase(nome.charAt(0)) + nome.substring(1);
+
         System.out.println("Bene, " + nome + ", qual'è il tuo cognome?");
         String cognome = input.nextLine();
+        cognome = Character.toUpperCase(cognome.charAt(0)) + cognome.substring(1);
+
         System.out.println("Bene, " + nome + " " + cognome + ", qual'è il tuo colore preferito?");
         String colore = input.nextLine();
+        colore = Character.toUpperCase(colore.charAt(0)) + colore.substring(1);
+
         System.out.println("Bene, ora inserisci la tua data di nascita, partiamo dal giorno");
         int giorno = input.nextInt();
-        System.out.println("Bene, ora inserisci la tua data di nascita, procediamo con il mese (non usare zeri prima del mese)");
+        System.out.println("Bene, ora inserisci la tua data di nascita, procediamo con il mese");
         int mese = input.nextInt();
         System.out.println("Bene, ora inserisci la tua data di nascita, procediamo con l'anno");
         int anno = input.nextInt();
